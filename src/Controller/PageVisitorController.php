07728fac8a875e7096653bfe2fd4c18bdb00d9ea
Allan B <allan.biville@outlook.fr>
2021-11-22 17:31:25 +0100

    Add user interface

---
+++ src/Controller/PageVisitorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageVisitorController extends AbstractController
+{
+    /**
+     * @Route("/visitor", name="visitor")
+     */
+    public function index(): Response
+    {
+        return $this->render('page_visitor/index.html.twig', [
+            'controller_name' => 'PageVisitorController',
+        ]);
+    }
+}
