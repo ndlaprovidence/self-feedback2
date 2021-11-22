b15012f5fb19a40601c9dd00916d00f74d0b9dd7
Allan B <allan.biville@outlook.fr>
2021-11-22 19:16:30 +0100

    [CORE] - Add Login

--- src/Entity/Avis.php
+++ src/Entity/Avis.php
@@ -16,12 +16,40 @@ class Avis
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $gout;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $diversite;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $chaleur;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $disponibilite;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $proprete;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $acceuil;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $commentaire;
 
     public function getId(): ?int
@@ -32,140 +60,90 @@ class Avis
     /**
      * Get the value of diversite
      */ 
-    public function getDiversite(): ?int
+
+    public function getGout(): ?int
     {
-        return $this->diversite;
+        return $this->gout;
     }
 
-    /**
-     * Set the value of diversite
-     *
-     * @return  self
-     */ 
-    public function setdDiversite($diversite)
-    {
-        $this->diversite = $diversite;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of chaleur
-     */ 
-    public function getChaleur() :?int
-    {
-        return $this->chaleur;
-    }
-
-    /**
-     * Set the value of chaleur
-     *
-     * @return  self
-     */ 
-    public function setChaleur($chaleur)
-    {
-        $this->chaleur = $chaleur;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of disponibilite
-     */ 
-    public function getDisponibilite(): ?int
-    {
-        return $this->disponibilite;
-    }
-
-    /**
-     * Set the value of disponibilite
-     *
-     * @return  self
-     */ 
-    public function setDisponibilite($disponibilite)
-    {
-        $this->disponibilite = $disponibilite;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of proprete
-     */ 
-    public function getProprete(): ?int
-    {
-        return $this->proprete;
-    }
-
-    /**
-     * Set the value of proprete
-     *
-     * @return  self
-     */ 
-    public function setProprete($proprete)
-    {
-        $this->proprete = $proprete;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of acceuil
-     */ 
-    public function getAcceuil(): ?int
-    {
-        return $this->acceuil;
-    }
-
-    /**
-     * Set the value of acceuil
-     *
-     * @return  self
-     */ 
-    public function setAcceuil($acceuil)
-    {
-        $this->acceuil = $acceuil;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of commentaire
-     */ 
-    public function getCommentaire()
-    {
-        return $this->commentaire;
-    }
-
-    /**
-     * Set the value of commentaire
-     *
-     * @return  self
-     */ 
-    public function setCommentaire($commentaire)
-    {
-        $this->commentaire = $commentaire;
-
-        return $this;
-    }
-
-    /**
-     * Set the value of _gout
-     *
-     * @return  self
-     */ 
-    public function setGout($gout)
+    public function setGout(int $gout): self
     {
         $this->gout = $gout;
 
         return $this;
     }
 
-    /**
-     * Get the value of _gout
-     */ 
-    public function getGout(): ?int
+    public function getDiversite(): ?int
     {
-        return $this->gout;
+        return $this->diversite;
     }
+
+    public function setDiversite(int $diversite): self
+    {
+        $this->diversite = $diversite;
+
+        return $this;
+    }
+
+    public function getChaleur(): ?int
+    {
+        return $this->chaleur;
+    }
+
+    public function setChaleur(int $chaleur): self
+    {
+        $this->chaleur = $chaleur;
+
+        return $this;
+    }
+
+    public function getDisponibilite(): ?int
+    {
+        return $this->disponibilite;
+    }
+
+    public function setDisponibilite(int $disponibilite): self
+    {
+        $this->disponibilite = $disponibilite;
+
+        return $this;
+    }
+
+    public function getProprete(): ?int
+    {
+        return $this->proprete;
+    }
+
+    public function setProprete(int $proprete): self
+    {
+        $this->proprete = $proprete;
+
+        return $this;
+    }
+
+    public function getAcceuil(): ?int
+    {
+        return $this->acceuil;
+    }
+
+    public function setAcceuil(int $acceuil): self
+    {
+        $this->acceuil = $acceuil;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+   
 }
