63710526f4e56430454c9b0b9fc06ac3c85669e7
LAHEYNE Sebastien <laheseb@ndlp.fr>
2021-11-25 12:00:11 +0100

    [CORE] PageVisitor

--- src/Form/PageVisitorType.php
+++ src/Form/PageVisitorType.php
@@ -4,63 +4,80 @@ namespace App\Form;
 
 use App\Entity\Avis;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PageVisitorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $choise =[
+        $choise = [
             '★1' => '1',
             '★2' => '2',
             '★3' => '3',
             '★4' => '4',
-            '★5' => '5'
+            '★5' => '5',
         ];
         $builder
             ->add('gout'
-            , ChoiceType::class,[
-                
-                'choices' => $choise
+                , ChoiceType::class, [
+                    'label'=>'Goût du plat',
+                    'attr' => ['class' => 'star-ratings-input'],
+                    
+                    'attr' => ['class' => 'stars'],
+                    
+                    'choices' => $choise,
 
-            ])
+                    'expanded' => true,
+                    'multiple' => false,
+
+                ])
             ->add('diversite'
-            , ChoiceType::class,[
-                
-                'choices' => $choise
+                , ChoiceType::class, [
 
-            ])
+                    'choices' => $choise,
+
+                    'expanded' => true,
+                    'multiple' => false,
+
+                ])
             ->add('chaleur'
-            , ChoiceType::class,[
-                
-                'choices' => $choise
+                , ChoiceType::class, [
 
-            ])
+                    'choices' => $choise,
+                    'expanded' => true,
+                    'multiple' => false,
+
+                ])
             ->add('disponibilite'
-            , ChoiceType::class,[
-                
-                'choices' => $choise
+                , ChoiceType::class, [
 
-            ])
+                    'choices' => $choise,
+                    'expanded' => true,
+                    'multiple' => false,
+
+                ])
             ->add('proprete'
-            , ChoiceType::class,[
-                
-                'choices' => $choise
+                , ChoiceType::class, [
+
+                    'choices' => $choise,
+                    'expanded' => true,
+                    'multiple' => false,
+
+                ])
+
+            ->add('acceuil', ChoiceType::class, [
+
+                'choices' => $choise,
+                'expanded' => true,
+                'multiple' => false,
 
             ])
-            ->add('acceuil'
-            , ChoiceType::class,[
-                
-                'choices' => $choise
 
-            ])
             ->add('commentaire')
-            ->add('save', SubmitType::class) 
+            ->add('save', SubmitType::class)
         ;
     }
 
