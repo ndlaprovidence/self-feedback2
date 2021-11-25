b6d953df00bf99314e3f667086cd409c4232a183
JENOUVRIER Hugo <jenohug@ndlp.fr>
2021-11-25 16:59:04 +0100

    Add graphics gout

--- src/Controller/GraphController.php
+++ src/Controller/GraphController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AvisRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,17 +27,39 @@ class GraphController extends AbstractController
      */
     public function dashboard(AvisRepository $avisRepository)
     {
+        $gout1 = $avisRepository->countgout(1);
+        $gout2 = $avisRepository->countgout(2);
+        $gout3 = $avisRepository->countgout(3);
+        $gout4 = $avisRepository->countgout(4);
+        $gout5 = $avisRepository->countgout(5);
+
+
         $data = [
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'labels' => ['1 étoiles','2 étoiles','3 étoiles','4 étoiles','5 étoiles'],
             'datasets' => [
                 [
-                    'label' => 'Sales!',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [522, 1500, 2250, 2197, 2345, 3122, 3099],
+                    'label' => 'Gout!',
+                    'backgroundColor' => [
+                        'rgba(255, 0, 0)',
+                        'rgba(255, 71, 9)',
+                        'rgba(255, 174, 9)',
+                        'rgba(166, 217, 79)',
+                        'rgba(82, 192, 0)'],
+                    'borderColor' => [
+                        'rgba(255, 0, 0)',
+                        'rgba(255, 71, 9)',
+                        'rgba(255, 174, 9)',
+                        'rgba(166, 217, 79)',
+                        'rgba(82, 192, 0)'],
+                    'data' => [
+                        $gout1[1],$gout2[1],$gout3[1],$gout4[1],$gout5[1]
+                    ],
                 ],
             ],
         ];
+
+
+
         return $this->render('graph/index.html.twig', [
             'data' => json_encode($data),
         ]);
--- src/Repository/AvisRepository.php
+++ src/Repository/AvisRepository.php
@@ -19,7 +19,26 @@ class AvisRepository extends ServiceEntityRepository
         parent::__construct($registry, Avis::class);
     }
 
+    public function countgout($value)
+    {
+        // return $this->createQueryBuilder('a')
+        //     ->andWhere('a.gout = :val')
+        //     ->setParameter('val', $value)
+        //     ->getQuery()
+        //     ->getResult()
+        // ;
+        $entityManager = $this->getEntityManager();
 
+        $query = $entityManager->createQuery(
+            'SELECT count(a.gout)
+            FROM App\Entity\Avis a
+            WHERE a.gout = :value'
+        )->setParameter('value', $value);
+
+        // returns an array of Product objects
+        return $query->getOneOrNullResult();
+        
+    }
 
     
     // /**
@@ -27,6 +46,8 @@ class AvisRepository extends ServiceEntityRepository
     //  */
     /*
     public function findByExampleField($value)
+    {
+            public function findByExampleField($value)
     {
         return $this->createQueryBuilder('a')
             ->andWhere('a.exampleField = :val')
@@ -36,6 +57,7 @@ class AvisRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult()
         ;
+    }
     }
     */
 
