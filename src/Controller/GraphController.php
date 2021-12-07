57afdfc5af474b130592889d477c44e48be22ad5
JENOUVRIER Hugo <jenohug@ndlp.fr>
2021-12-07 15:48:08 +0100

    Add tcpdf test

--- src/Controller/GraphController.php
+++ src/Controller/GraphController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
-use App\Repository\AvisRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/graph", name="")
@@ -25,197 +24,115 @@ class GraphController extends AbstractController
     /**
      * @Route("/dashboard", name="graph_dashboard")
      */
-    public function dashboard(AvisRepository $avisRepository)
+    public function dashboard()
     {
-        $gout1 = $avisRepository->countgout(1);
-        $gout2 = $avisRepository->countgout(2);
-        $gout3 = $avisRepository->countgout(3);
-        $gout4 = $avisRepository->countgout(4);
-        $gout5 = $avisRepository->countgout(5);
 
-        $diver1 = $avisRepository->countdiver(1);
-        $diver2 = $avisRepository->countdiver(2);
-        $diver3 = $avisRepository->countdiver(3);
-        $diver4 = $avisRepository->countdiver(4);
-        $diver5 = $avisRepository->countdiver(5);
+        $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('Hugo JENOUVRIER');
+        $pdf->SetTitle('Avis 01');
+        $pdf->SetSubject('Avis sur le self');
+
+        // set header and footer fonts
+        $pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+        // set auto page breaks
+        $pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        // set some language-dependent strings (optional)
+        if (@file_exists(dirname(__FILE__) . '/lang/eng.php')) {
+            require_once dirname(__FILE__) . '/lang/eng.php';
+            $pdf->setLanguageArray($l);
+        }
+
+        // ---------------------------------------------------------
+
+        // set font
+        $pdf->SetFont('helvetica', 'B', 20);
+
+        // add a page
+        $pdf->AddPage();
+
+        $pdf->Write(0, 'Accueil');
+
+        $xc = 105;
+        $yc = 100;
+        $r = 50;
+
+        $pdf->SetFillColor(0, 0, 255);
+        $pdf->PieSector($xc, $yc, $r, 20, 120, 'FD', false, 0, 2);
+
+        $pdf->SetFillColor(0, 255, 0);
+        $pdf->PieSector($xc, $yc, $r, 120, 250, 'FD', false, 0, 2);
+
+        $pdf->SetFillColor(255, 0, 0);
+        $pdf->PieSector($xc, $yc, $r, 250, 20, 'FD', false, 0, 2);
+
+        $pdf->SetFillColor(255, 255, 0);
+        $pdf->PieSector($xc, $yc, $r, 200, 250, 'FD', false, 0, 2);
+
         
-        $chaleur1 = $avisRepository->countchaleur(1);
-        $chaleur2 = $avisRepository->countchaleur(2);
-        $chaleur3 = $avisRepository->countchaleur(3);
-        $chaleur4 = $avisRepository->countchaleur(4);
-        $chaleur5 = $avisRepository->countchaleur(5);
-        
-        $dispo1 = $avisRepository->countdispo(1);
-        $dispo2 = $avisRepository->countdispo(2);
-        $dispo3 = $avisRepository->countdispo(3);
-        $dispo4 = $avisRepository->countdispo(4);
-        $dispo5 = $avisRepository->countdispo(5);
-        
-        $propr1 = $avisRepository->countpropr(1);
-        $propr2 = $avisRepository->countpropr(2);
-        $propr3 = $avisRepository->countpropr(3);
-        $propr4 = $avisRepository->countpropr(4);
-        $propr5 = $avisRepository->countpropr(5);
+        // write labels
+        $pdf->SetTextColor(255, 255, 255);
+        $pdf->Text(105, 65, '1');
+        $pdf->Text(60, 95, '2');
+        $pdf->Text(120, 115, '3');
+        $pdf->Text(80, 115, '4');
 
-        $accueil1 = $avisRepository->countaccueil(1);
-        $accueil2 = $avisRepository->countaccueil(2);
-        $accueil3 = $avisRepository->countaccueil(3);
-        $accueil4 = $avisRepository->countaccueil(4);
-        $accueil5 = $avisRepository->countaccueil(5);
 
-        $data_gout = [
-            'labels' => ['1 étoiles','2 étoiles','3 étoiles','4 étoiles','5 étoiles'],
-            'datasets' => [
-                [
-                    'label' => 'Gout !',
-                    'backgroundColor' => [
-                        'rgba(255,1,0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'borderColor' => [
-                        'rgba(255, 0, 0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'data' => [
-                        $gout1[1],$gout2[1],$gout3[1],$gout4[1],$gout5[1]
-                    ],
-                ],
-            ],
-        ];
 
-        $data_diversite = [
-            'labels' => ['1 étoiles','2 étoiles','3 étoiles','4 étoiles','5 étoiles'],
-            'datasets' => [
-                [
-                    'label2' => 'Diversité !',
-                    'backgroundColor' => [
-                        'rgba(255,1,0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'borderColor' => [
-                        'rgba(255, 0, 0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'data' => [
-                        $diver1[1],$diver2[1],$diver3[1],$diver4[1],$diver5[1]
-                    ],
-                ],
-            ],
-        ];
 
-        $data_chaleur = [
-            'labels' => ['1 étoiles','2 étoiles','3 étoiles','4 étoiles','5 étoiles'],
-            'datasets' => [
-                [
-                    'label2' => 'Chaleur !',
-                    'backgroundColor' => [
-                        'rgba(255,1,0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'borderColor' => [
-                        'rgba(255, 0, 0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'data' => [
-                        $chaleur1[1],$chaleur2[1],$chaleur3[1],$chaleur4[1],$chaleur5[1]
-                    ],
-                ],
-            ],
-        ];
+        // $pdf->Image($image_file, 10, 10, 20, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        $pdf->SetTextColor(0, 0, 0);
+        // Set font
+        $pdf->SetFont('helvetica', 'B', 40);
+        // Title
+        $pdf->SetXY(0, 0);
+        $pdf->Cell(210, 50, 'FeedBack 01 ', 0, false, 'C');
 
-        $data_disponibilite = [
-            'labels' => ['1 étoiles','2 étoiles','3 étoiles','4 étoiles','5 étoiles'],
-            'datasets' => [
-                [
-                    'label2' => 'Disponibilité !',
-                    'backgroundColor' => [
-                        'rgba(255,1,0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'borderColor' => [
-                        'rgba(255, 0, 0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'data' => [
-                        $dispo1[1],$dispo2[1],$dispo3[1],$dispo4[1],$dispo5[1]
-                    ],
-                ],
-            ],
-        ];
+        // ---------------------------------------------------------
 
-        $data_proprete = [
-            'labels' => ['1 étoiles','2 étoiles','3 étoiles','4 étoiles','5 étoiles'],
-            'datasets' => [
-                [
-                    'label2' => 'Propreté !',
-                    'backgroundColor' => [
-                        'rgba(255,1,0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'borderColor' => [
-                        'rgba(255, 0, 0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'data' => [
-                        $propr1[1],$propr2[1],$propr3[1],$propr4[1],$propr5[1]
-                    ],
-                ],
-            ],
-        ];
-
-        $data_accueil = [
-            'labels' => ['1 étoiles','2 étoiles','3 étoiles','4 étoiles','5 étoiles'],
-            'datasets' => [
-                [
-                    'label2' => 'Accueil !',
-                    'backgroundColor' => [
-                        'rgba(255,1,0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'borderColor' => [
-                        'rgba(255, 0, 0)',
-                        'rgba(255, 71, 9)',
-                        'rgba(255, 174, 9)',
-                        'rgba(166, 217, 79)',
-                        'rgba(82, 192, 0)'],
-                    'data' => [
-                        $accueil1[1],$accueil2[1],$accueil3[1],$accueil4[1],$propr5[1]
-                    ],
-                ],
-            ],
-        ];
-
+        //Close and output PDF document
+        $pdf->Output('example_031.pdf', 'I');
         return $this->render('graph/index.html.twig', [
-            'data_gout' => json_encode($data_gout),
-            'data_diversite' => json_encode($data_diversite),
-            'data_chaleur' => json_encode($data_chaleur),
-            'data_disponibilite' => json_encode($data_disponibilite),
-            'data_proprete' => json_encode($data_proprete),
-            'data_accueil' => json_encode($data_accueil),
 
-            
         ]);
     }
 }
+// class MYPDF extends \TCPDF {
+
+//     //Page header
+//     public function Header() {
+//         // Logo
+//         $image_file = K_PATH_IMAGES.'ndlp.jpg';
+//         $this->Image($image_file, 10, 10, 20, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+//         // Set font
+//         $this->SetFont('helvetica', 'B', 20);
+//         // Title
+//         $this->Cell(0, 15, 'FeedBack 01 ', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+//     }
+
+//     // Page footer
+//     public function Footer() {
+//         // Position at 15 mm from bottom
+//         $this->SetY(-15);
+//         // Set font
+//         $this->SetFont('helvetica', 'I', 8);
+//         // Page number
+//         $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+//     }
+// }
