4771c2660a385781fb62e243f1d3164f56bb8ce7
BIVILLE Allan <biviall@ndlp.fr>
2021-12-14 16:08:03 +0100

    DesTrucsCoolMarcheEtDesTrucsNeMaarchePas

--- src/Controller/PageVisitorController.php
+++ src/Controller/PageVisitorController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Avis;
 use App\Form\PageVisitorType;
 use App\Entity\TypesUtilisateurs;
+use App\Repository\TypesRepasRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\TypesUtilisateursRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -28,17 +30,32 @@ class PageVisitorController extends AbstractController
 
         return $this->render('page_visitor/index.html.twig',[
             'form' => $form->createView(),
+
         ]);    
     }
     
     /**
      * @Route("/save", name="visitor_save")
      */
-    public function save(EntityManagerInterface $entityManager, Request $request): Response
+    public function save(EntityManagerInterface $entityManager, Request $request, TypesUtilisateursRepository $typesUtilisateursRepository, TypesRepasRepository $typesRepasRepository ): Response
     {
+
+        $page_visitor= $request->get('page_visitor');
+        $user_type_id = $page_visitor['user_type'];
+        $unTypesUtilisateurs = $typesUtilisateursRepository->findOneById($user_type_id);
+
+        $page_visitor= $request->get('page_visitor');
+        $repas_type_id = $page_visitor['repas_type'];
+        $unTypesRepas = $typesRepasRepository->findOneById($repas_type_id);
+
+        //$classe = $_GET['[user_type]'];
+        //$classe = $_GET['user_type'];
+        // $classe = $_GET['page_visitor_user_type'];
+        // $classe = $_GET['page_visitor_user_type_'];
+        // $classe = $_GET['page_visitoruser_type'];
+        // $classe = $_GET['page_visitor[user_type]'];
         $avis = new Avis();
-        $classe = new TypesUtilisateurs();
-        
+        //$class = new TypesUtilisateurs();
         $avis->setProprete($request->get('note-proprete'));
         $avis->setGout($request->get('note-gout'));
         $avis->setDiversite($request->get('note-diversite'));
@@ -46,8 +63,10 @@ class PageVisitorController extends AbstractController
         $avis->setDisponibilite($request->get('note-disponibilite'));
         $avis->setChaleur($request->get('note-chaleur'));
         $avis->setCommentaire($request->get('commentaire'));
-        $classe->setLibelleT($request->get('libelle_t'));
-        $entityManager->persist($avis,$classe);
+        $avis->setTypesUtilisateurs($unTypesUtilisateurs);
+        $avis->setRepas($unTypesRepas);
+        dump();
+        $entityManager->persist($avis);
         $entityManager->flush();
         return $this->render('page_visitor/save.html.twig');    
     }
--- src/Form/PageVisitorType.php
+++ src/Form/PageVisitorType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Avis;
+use App\Entity\TypesRepas;
 use App\Entity\TypesUtilisateurs;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -14,21 +16,27 @@ class PageVisitorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     { 
-        $choise = [
-            'Etudiant' => 'Etudiant',
-            'Terminale' => 'Terminale',
-            'Premiere' => 'v',
-            'Seconde' => 'h',
-            'Troisieme' => 'aaa',
-        ];
-        $builder
-            ->add('libelle_t'
-                , ChoiceType::class, [
-                    'choices' => $choise,
-                ])
-            
-  
-                ;
+        // $choise = [
+        //     'Etudiant' => '1',
+        //     'Terminale' => '2',
+        //     'Premiere' => '3',
+        //     'Seconde' => '4',
+        //     'Troisieme' => '5',
+        //     'Personnel' => '6',
+        // ];
+        // $builder
+        //     ->add('Classe'
+        //         , ChoiceType::class, [
+        //             'choices' => $choise,
+        //         ]);
+        $builder->add('user_type', EntityType::class, [
+            'class' => TypesUtilisateurs::class,
+            'choice_label' => 'libelle_t',           
+        ])
+        ->add('repas_type', EntityType::class, [
+            'class' => TypesRepas::class,
+            'choice_label' => 'libelle_r',
+        ]);
 
     }
 
